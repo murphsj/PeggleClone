ea646e08ba11ae4abeef8a3a2443e4308d618d5b
Carlton Luu <153663592+MelvinTheMonster@users.noreply.github.com>
2025-10-12 18:10:06 -0400

    Update GoalPeg.java

--- src/game/GoalPeg.java
+++ src/game/GoalPeg.java
@@ -7,12 +7,21 @@ public class GoalPeg extends Peg {
 	 * The base color of this sprite.
 	 */
 	private static final Color COLOR_GOAL_PEG = new Color(0.32f, 0.40f, 0.81f);
-	
+
+	/**
+	 * Builds a circular polygonal shape with the given position
+	 * @param position 
+	 */
     public GoalPeg(Point position) {
         super(position, COLOR_GOAL_PEG); 
     }
-
+	
+	/**
+	 * Increases score by 1 when hit and lights up
+	 * @author Carlton Luu 
+	 * @return void
+	 */
     public void onHit() {
-
+		
     }
 }
